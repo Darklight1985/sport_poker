083b096d78a34979e913025afa43c5ccabde7e60
Dmitrij Kolesnev <darklight1985@mail.ru>
2025-05-28 22:09:00 +0300

    Добавил настройку тест контейнера (#5)

--- src/test/java/ru/poker/sportpoker/TestcontainersConfiguration.java
+++ src/test/java/ru/poker/sportpoker/TestcontainersConfiguration.java
@@ -3,15 +3,20 @@ package ru.poker.sportpoker;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-//    @Bean
-//    @ServiceConnection
-//    PostgreSQLContainer<?> postgresContainer() {
-//        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-//    }
+    @Bean
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:13")
+                .withDatabaseName("integration-tests-db")
+                .withUsername("sa")
+                .withPassword("password");
+        container.start();
+        return container;
+    }
 
 }
