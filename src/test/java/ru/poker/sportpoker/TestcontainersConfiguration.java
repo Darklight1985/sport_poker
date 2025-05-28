972e33904a5462bbfe795a586edce10a114a7a60
Darklight1985 <darklight1985@mail.ru>
2025-05-28 22:35:42 +0300

    Доработал настройку

--- src/test/java/ru/poker/sportpoker/TestcontainersConfiguration.java
+++ src/test/java/ru/poker/sportpoker/TestcontainersConfiguration.java
@@ -3,20 +3,33 @@ package ru.poker.sportpoker;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-    @Bean
-    public PostgreSQLContainer<?> postgreSQLContainer() {
-        PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:13")
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
+
+    static {
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:13")
                 .withDatabaseName("integration-tests-db")
                 .withUsername("sa")
                 .withPassword("password");
-        container.start();
-        return container;
+        POSTGRESQL_CONTAINER.start();
     }
 
+    @Bean
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return POSTGRESQL_CONTAINER;
+    }
+
+    @DynamicPropertySource
+    static void registerPostgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
+    }
 }
