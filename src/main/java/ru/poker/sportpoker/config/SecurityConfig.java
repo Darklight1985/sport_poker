a087c5720463786e1680dffb82f20f6384af5861
Darklight1985 <darklight1985@mail.ru>
2025-06-24 21:03:01 +0300

    Открыл CORS временно

--- src/main/java/ru/poker/sportpoker/config/SecurityConfig.java
+++ src/main/java/ru/poker/sportpoker/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // ваш фронт
+                        .allowedOrigins("*") // ваш фронт
                         .allowedMethods("*");
             }
         };
