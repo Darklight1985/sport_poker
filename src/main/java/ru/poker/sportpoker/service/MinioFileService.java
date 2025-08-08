65f0b5d68072ad69483aaa93bb48d3cf892481c3
Darklight1985 <darklight1985@mail.ru>
2025-08-08 19:31:48 +0300

    Правка minio

--- src/main/java/ru/poker/sportpoker/service/MinioFileService.java
+++ src/main/java/ru/poker/sportpoker/service/MinioFileService.java
@@ -15,7 +15,6 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 @Service
