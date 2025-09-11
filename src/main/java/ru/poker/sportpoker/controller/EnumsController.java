28d60ecb67a9088319f7a78c2031e57e68f495ba
Darklight1985 <darklight1985@mail.ru>
2025-09-11 22:04:11 +0300

    Добавил контроллер загрузки карт

--- src/main/java/ru/poker/sportpoker/controller/EnumsController.java
+++ src/main/java/ru/poker/sportpoker/controller/EnumsController.java
@@ -3,7 +3,10 @@ package ru.poker.sportpoker.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.poker.sportpoker.dto.EnumDto;
@@ -25,5 +28,11 @@ public class EnumsController {
     @GetMapping("/exercises")
     public List<EnumDto> getExercises () {
         return enuMapper.getExercises(Arrays.stream(Exercises.values()).toList());
+    }
+
+    @Operation(description = "Получение изображения карты")
+    @GetMapping("/cards/{suit}/{card}")
+    public ResponseEntity<InputStreamResource> getCard (@PathVariable String suit, String card) {
+        return null;
     }
 }
